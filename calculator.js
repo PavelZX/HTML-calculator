c6e2fb8d561f59408af5caba0e3cce776aed8cf6
Pavel Zachesa <uefpro@gmail.com>
2020-08-07 12:33:25 +0300

    Изменил кавычки, на правильные

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
-const calculator = document.querySelector(‘.calculator’)
-const keys = calculator.querySelector(‘.calculator__keys’)
-keys.addEventListener(‘click’, e => {
- if (e.target.matches(‘button’)) {
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+keys.addEventListener('click', e => {
+ if (e.target.matches('button')) {
    // Do something
  }
 })
@@ -31,3 +31,27 @@ console.log('clear key!')
 if (action === 'calculate') {
 console.log('equal key!')
 }
+
+const display = document.querySelector('.calculator__display')
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    // ...
+  }
+})
+
+if (!action) {
+    if (displayedNum === '0') {
+      display.textContent = keyContent
+    } else {
+      display.textContent = displayedNum + keyContent
+    }
+}
+
+if (action === 'decimal') {
+    display.textContent = displayedNum + '.'
+}
+
